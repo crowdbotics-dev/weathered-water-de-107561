2110b08618e42b388d685197807733ea996c3c5f
Crowdbotics <engineering@crowdbotics.com>
2023-08-06 23:18:26 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "weathered-water-de-107561",
+  url: "https://weathered-water-de-107561.botics.co",
+  api: "https://weathered-water-de-107561.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
